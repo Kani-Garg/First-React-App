a5dac45411002838100ec2e6d17d642885578658
Kani Singhal <singhal.kani@thinksys.com>
2018-08-23 14:21:23 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Mycomponent extends React.Component {
+  render() {
+    console.log(module);
+    return <div> hello My first component</div>;
+  }
+}
+export default Mycomponent;
+export const abs = "test";
